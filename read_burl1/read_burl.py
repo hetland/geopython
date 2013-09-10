05f919ad61bd1b88d8c5e2f8e1248060790621e5
Demonstration <hetland@tamu.edu>
2013-09-10 14:15:19 -0500

    small updates to burl1 data

--- read_burl1/read_burl.py
+++ read_burl1/read_burl.py
@@ -3,17 +3,27 @@
 # 2013-09-03
 
 import numpy as np
+from datetime import datetime
 
 f = open('burl1_2011.dat')
 
+dates = []
 pressure = []
 
 for line in f.readlines()[2:]:
     data = line.split()
+    year = int(data[0])
+    dates.append(datetime(year, 1, 1))
     pressure.append( float(data[12]) )
 
-pressure = np.array(pressure)
+# pressure = np.array(pressure)
+# dates = np.array(dates)
 
-print 'Mean pressure = ', pressure.mean()
-print 'Min pressure = ', pressure.min()
-print 'Max pressure = ', pressure.max()
+data = {'dates': np.array(dates), 'pressure': np.array(pressure)}
+print data['dates']
+print data['pressure']
+
+
+# print 'Mean pressure = ', pressure.mean()
+# print 'Min pressure = ', pressure.min()
+# print 'Max pressure = ', pressure.max()
